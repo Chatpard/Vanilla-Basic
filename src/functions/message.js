db26260f677549a9eaf49b6b195953737bec9f94
Christian <moenaert@hotmail.com>
2023-11-24 13:54:08 +0100

    update

--- src/functions/message.js
+++ src/functions/message.js
@@ -3,7 +3,6 @@ const { app } = require('@azure/functions');
 app.http('message', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
-<<<<<<< HEAD
     route: '{name}',
     handler: async (request, context) => {
         //context.log(`Http function processed request for url "${request.url}"`);
@@ -15,13 +14,3 @@ app.http('message', {
         };
     }
 });
-=======
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
-
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
-    }
-});
->>>>>>> 08b252a1a803f77b19c16126ad6e7545123ef509
