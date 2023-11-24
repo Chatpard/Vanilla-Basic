c09096a12067839d2eafb192174e23b0ab6443ea
Christian <moenaert@hotmail.com>
2023-11-24 11:05:05 +0100

    jsonBody

---
+++ src/functions/message.js
@@ -0,0 +1,16 @@
+const { app } = require('@azure/functions');
+
+app.http('message', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    route: '{name}',
+    handler: async (request, context) => {
+        //context.log(`Http function processed request for url "${request.url}"`);
+
+        //const name = request.query.get('name') || await request.text() || 'world';
+
+        return {
+            jsonBody: { "message": `Hello, ${request.params.name}` }
+        };
+    }
+});
